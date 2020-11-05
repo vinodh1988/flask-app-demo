976075f17b3cad51f77aae631cba750c8b0520e0
vinodh1988 <k.c.vinodh@hotmail.com>
2020-11-05 12:18:51 +0530

    rest-api

--- routes/home.py
+++ routes/home.py
@@ -1,5 +1,5 @@
 from config import app
-from flask import render_template,request,redirect,url_for
+from flask import render_template,request,redirect,url_for,jsonify
 from databases.models.applicants import Applicant,db 
 
 @app.route("/")
@@ -23,4 +23,26 @@ def register():
    
     #return "fname: {x}, gender: {y}, city: {z}".format(x=fname,y=gender,z=city)
     return home()
+
+@app.route("/applicants")
+def applicants():
+    try:
+        applicants = Applicant.query.all()
+        applicants = [ x.serialize() for x in applicants]
+        return jsonify(applicants), 200
+    except:
+        return jsonify({'msg':"server error"}) , 500
+
+@app.route("/applicants", methods=['post'])
+def store():
+    try:
+        data = request.get_json();
+        applicant = Applicant(data['fname'],data['gender'],data['city'])
+        db.session.add(applicant) # will only add in object layer not in db
+        db.session.commit() # all the objects created/updated will be commited to database
+        print(data)
+        return jsonify({'msg':"stored data successfully"}), 200
+    except:
+        return jsonify({'msg':"server error"}) , 500
+
 
