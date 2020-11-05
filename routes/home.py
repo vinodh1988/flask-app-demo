c125edc335f7a6bf3027b79dc25cba4dfc765d2f
vinodh1988 <k.c.vinodh@hotmail.com>
2020-11-05 09:20:50 +0530

    form-creationg

--- routes/home.py
+++ routes/home.py
@@ -3,4 +3,4 @@ from flask import render_template
 
 @app.route("/")
 def home():
-    return render_template("index.html", author ="Vinodh Kumar")
+    return render_template("index.html", name ="Micro web application")
