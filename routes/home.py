ec4eb9afe9a886c13b578a71673dc440ef53be18
vinodh1988 <k.c.vinodh@hotmail.com>
2020-11-05 09:48:06 +0530

    form post

--- routes/home.py
+++ routes/home.py
@@ -1,6 +1,15 @@
 from config import app
-from flask import render_template
+from flask import render_template,request
 
 @app.route("/")
 def home():
     return render_template("index.html", name ="Micro web application")
+
+
+@app.route("/register", methods=['post'])
+def register():
+    fname = request.form['fname']
+    gender = request.form['gender']
+    city = request.form['city']
+    return fname+" "+gender+" "+city
+
