95727770f2845b1a7ff04c5509451f2be1ecb4df
vinodh1988 <k.c.vinodh@hotmail.com>
2020-11-05 11:41:31 +0530

    jinja-table-serialization

--- routes/home.py
+++ routes/home.py
@@ -1,10 +1,14 @@
 from config import app
-from flask import render_template,request
+from flask import render_template,request,redirect,url_for
 from databases.models.applicants import Applicant,db 
 
 @app.route("/")
 def home():
-    return render_template("index.html", name ="Micro web application")
+    applicants = Applicant.query.all()
+    print(applicants)
+    applicants = [ x.serialize() for x in applicants]
+    print(applicants)
+    return render_template("index.html", state ={'name':"Micro web application",'applicants':applicants})
 
 
 @app.route("/register", methods=['post'])
@@ -16,5 +20,7 @@ def register():
     
     db.session.add(applicant) # will only add in object layer not in db
     db.session.commit() # all the objects created/updated will be commited to database
-    return "fname: {x}, gender: {y}, city: {z}".format(x=fname,y=gender,z=city)
+   
+    #return "fname: {x}, gender: {y}, city: {z}".format(x=fname,y=gender,z=city)
+    return home()
 
